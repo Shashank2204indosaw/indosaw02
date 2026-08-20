01b7890f615894ffbd6c7b98229ff114d32048af
Shashank Nautiyal <research10@indosaw.in>
2026-08-20 14:42:49 +0530

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,50 @@
+
+> Open this page at [https://shashank2204indosaw.github.io/indosaw01/](https://shashank2204indosaw.github.io/indosaw01/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/shashank2204indosaw/indosaw01** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/shashank2204indosaw/indosaw01** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://shashank2204indosaw.github.io/indosaw02/](https://shashank2204indosaw.github.io/indosaw02/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/shashank2204indosaw/indosaw02** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/shashank2204indosaw/indosaw02** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,146 @@
+// Dropdown menu restricted strictly to Pins 0 through 15
+enum BoardPin {
+    //% block="P0"
+    P0 = DigitalPin.P0,
+    //% block="P1"
+    P1 = DigitalPin.P1,
+    //% block="P2"
+    P2 = DigitalPin.P2,
+    //% block="P3"
+    P3 = DigitalPin.P3,
+    //% block="P4"
+    P4 = DigitalPin.P4,
+    //% block="P5"
+    P5 = DigitalPin.P5,
+    //% block="P6"
+    P6 = DigitalPin.P6,
+    //% block="P7"
+    P7 = DigitalPin.P7,
+    //% block="P8"
+    P8 = DigitalPin.P8,
+    //% block="P9"
+    P9 = DigitalPin.P9,
+    //% block="P10"
+    P10 = DigitalPin.P10,
+    //% block="P11"
+    P11 = DigitalPin.P11,
+    //% block="P12"
+    P12 = DigitalPin.P12,
+    //% block="P13"
+    P13 = DigitalPin.P13,
+    //% block="P14"
+    P14 = DigitalPin.P14,
+    //% block="P15"
+    P15 = DigitalPin.P15
+}
+
+// Dropdown menu for ON / OFF state
+enum PinState {
+    //% block="HIGH (1)"
+    High = 1,
+    //% block="LOW (0)"
+    Low = 0
+}
+
+//% color="#FF5722" icon="\uf0eb" block="My Signal"
+namespace mySignal {
+
+    /**
+     * Blink any chosen pin from P0 to P15
+     */
+    //% block="blink pin %pin for %delay ms"
+    //% delay.defl=500
+    //% weight=90
+    export function blinkBoardPin(pin: BoardPin, delay: number): void {
+        pins.digitalWritePin(pin as number, 1);
+        basic.pause(delay);
+        pins.digitalWritePin(pin as number, 0);
+        basic.pause(delay);
+    }
+
+}
+
+//% color="#1E88E5" icon="\uf2db" block="My Motors"
+namespace myMotors {
+
+    //% block="stop all motors"
+    export function stopMotors(): void {
+        pins.digitalWritePin(DigitalPin.P0, 0);
+        pins.digitalWritePin(DigitalPin.P1, 0);
+    }
+
+}
+
+//% color="#8E24AA" icon="\uf013" block="Device Configure"
+namespace DeviceConfig {
+
+    /**
+     * Configure any pin from P0 to P15 to HIGH or LOW
+     */
+    //% block="configure board pin %pin to state %state"
+    //% weight=95
+    export function setBoardPin(pin: BoardPin, state: PinState): void {
+        pins.digitalWritePin(pin as number, state);
+    }
+
+}
+
+//% color="#4CAF50" icon="\uf06c" block="Soil Sensor"
+namespace soilSensor {
+
+    /**
+     * Reads digital soil moisture level on Pin P4 (1 = Dry, 0 = Wet). 
+     * Automatically disables the LED display matrix so Pin P4 works as a clean input pin.
+     */
+    //% block="read soil sensor on P4"
+    //% weight=90
+    export function readSoilP4(): number {
+        led.enable(false);
+        return pins.digitalReadPin(DigitalPin.P4);
+    }
+
+    /**
+     * Reads digital soil moisture (1 = Dry, 0 = Wet). 
+     * Automatically turns off the LED display to allow pins like P4, P10 to work cleanly.
+     */
+    //% block="soil sensor on pin %pin is active"
+    //% weight=90
+    export function readSoil(pin: BoardPin): number {
+        led.enable(false);
+        return pins.digitalReadPin(pin as number);
+    }
+
+    /**
+     * Enable or disable the built-in 5x5 LED display screen
+     */
+    //% block="set LED matrix display %on"
+    //% on.shadow="toggleOnOff"
+    //% weight=80
+    export function setDisplay(on: boolean): void {
+        led.enable(on);
+    }
+
+}
+
+//% color="#FF9800" icon="\uf2c7" block="Temp Sensor"
+namespace tempSensor {
+
+    /**
+     * Reads temperature in Celsius from an external sensor on a chosen pin
+     */
+    //% block="read temperature (°C) on pin %pin"
+    //% weight=90
+    export function readTemperature(pin: BoardPin): number {
+        return dstemp.celsius(pin as number);
+    }
+
+    /**
+     * Reads temperature in Celsius directly from the micro:bit built-in CPU sensor
+     */
+    //% block="read internal micro:bit temperature (°C)"
+    //% weight=80
+    export function readInternalTemp(): number {
+        return input.temperature();
+    }
+
+}
